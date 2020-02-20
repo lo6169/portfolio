17db5c67ded5def23f9b9ca487644d922012fed0
lo6169 <43387417+lo6169@users.noreply.github.com>
2020-02-20 14:49:04 -0500

    Version 1.3.2 Updated the contact page again.

--- secure_email_form.php
+++ secure_email_form.php
@@ -1,37 +1,71 @@
 <?php
-    if(isset($_POST["submit"]))
+    function test_input($data)
     {
-        // Checking For Blank Fields..
-        if($_POST["vname"]==""||$_POST["vemail"]==""||$_POST["sub"]==""||$_POST["msg"]=="")
-        {
-            echo "Please fill out all fields";
-        }
-        else
-        {
-            // Check if the "Sender's Email" input field is filled out
-            $email = $_POST['vemail'];
+        $data = trim($data);
+        $data = stripslashes($data);
+        $ata = htmlspecialchars($data);
 
-            // Sanitize E-mail Address
-            $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-
-            // Validate E-mail Address
-            $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-            if (!$email)
-            {
-                echo "Please enter a valid email.";
-            }
-            else
-            {
-                $subject = $_POST['sub'];
-                $message = $_POST['msg'];
-                $headers = 'From:'. $email . "rn";
-                $headers .= 'Cc:'. $email . "rn";
-
-                $message = wordwrap($message, 70);
-
-                mail("lolsonform@gmail.com", $subject, $message, $headers);
-                echo "Your email was successfully sent, thank you!";
-            }
-        }
+        return $data;
     }
+
+    $vname  = $_POST['vname'];
+    $vemail = $_POST['vemail'];
+    $sub = $_POST['sub'];
+    $comment = $_POST['comment'];
+
+    $vname2 = test_input($vname);
+    $vemail2 = test_input($vemail);
+    $sub2 = test_input($sub);
+    $comment2 = test_input($comment);
+
+    $destination_email = "lolsonform@gmail.com";
+
+    $email_body = "$vemail2  -> $comment2";
+
+    mail($destination_email, $sub2, $email_body);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158851759-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-158851759-1');
+    </script>
+
+    <meta charset="UTF-8">
+    <title>Contact Me</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <nav id="page-nav">
+        <label for="hamburger">&#9776;</label>
+        <input type="checkbox" id="hamburger"/>
+
+        <ul>
+            <li><a href="index.html">Home</a></li>
+        </ul>
+    </nav>
+
+
+    <div class="head">
+        Thank you for your submission!
+    </div>
+
+    <div class="footer">
+        © Lindsey Olson - 2020 <br/> <br/>
+        Connect with me: <br/>
+        <a href="https://www.github.com/lo6169" target="_blank">
+            <img src="assets/media/githublogo.png" class="contactme" alt="GitHub logo">
+        </a>
+        <a href= "https://www.linkedin.com/in/l-olson" target="_blank">
+            <img src="assets/media/linkedinlogo.png" class="contactme" alt="LinkedIn logo">
+        </a>
+    </div>
+</body>
+</html>
